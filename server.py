59431309dc3a8825f02331d1192dc97cd7027081
BigosMaster16 <aleksanderkowalczyk1605@gmail.com>
2026-04-10 14:08:32 +0200

    zmiana modelu

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
 
-model = genai.GenerativeModel("gemini-3.1-flash-lite-preview")
+model = genai.GenerativeModel("gemini-1.5-flash-8b")
 
 @app.route("/generate", methods=["POST"])
 def generate_recipe():
